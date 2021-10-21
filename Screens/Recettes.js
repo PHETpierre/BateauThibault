f640856d92f9cb8a527241e95ac051bb6fa3b3f1
PHETpierre <36611009+PHETpierre@users.noreply.github.com>
2021-10-21 10:29:11 +0200

    update

--- Screens/Recettes.js
+++ Screens/Recettes.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 import RecettesJSON from '../Constantes/Recettes.json';
+import { globalStyle } from '../styles';
 
 var recettesList = RecettesJSON;
 
@@ -12,32 +13,60 @@ export default class Recettes extends React.Component {
 
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Vos recettes</Text>
-        <Text>
-        <p><strong>Toutes les recettes du bateau de Thibault</strong></p>
-        <p>06.55.66.77.88</p>
-        <p>addr@hotmail.com</p>
-        <p>www.facebookThibault.com</p>
-        </Text>
-        {recettesList.map((value, index)=>{
-          return <Button
-          title={value.name}
-          key={index}
-          onPress = {()=>{
-            this.props.navigation.navigate('Details de la recette', {recette:value});
-          }}>{value.name}</Button>
-        })}
+      <View style={globalStyle.container}>
+        <ImageBackground
+        source={require('../App_Resources/iOS/background.png')}
+        style={globalStyle.backgroundImage}
+        resizeMode="cover">
+          <View style={globalStyle.w50percent}>
+            <Text>Vos recettes</Text>
+            <Text>
+            <p><strong>Toutes les recettes du bateau de Thibault</strong></p>
+            <p>06.55.66.77.88</p>
+            <p>addr@hotmail.com</p>
+            <p>www.facebookThibault.com</p>
+            </Text>
+          </View>
+          <View style={globalStyle.w50percent, styles.container2} >
+            {recettesList.map((value, index)=>{
+              return(
+                <View style={[globalStyle.w50percent, styles.containerButton]}>
+                  <Button
+                  title={value.name}
+                  key={index}
+                  onPress = {()=>{
+                    this.props.navigation.navigate('Details de la recette', {recette:value});
+                  }}>{value.name}</Button>
+                </View>
+            )
+            })}
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container2: {
+    height: 200,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'start',
+  },
+  container3: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 10,
+  },
+  containerButton: {
+    padding: 10,
+  },
+  button: {
+    height: '20%',
+  },
+  buttonHello: {
+    width: 500,
+    backgroundColor: 'red',
   },
 });
--- styles.js
+++ styles.js
@@ -4,16 +4,22 @@ const globalStyle = StyleSheet.create({
   red: {
     color: 'red'
   },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
   backgroundImage: {
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  w50percent: {
+    width: '50%',
   },
 })
 
